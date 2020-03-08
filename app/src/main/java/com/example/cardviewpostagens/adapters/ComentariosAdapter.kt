936a5a9f6ddad9b24b39cb3d59502ee501bfb2e2
Cabomaldade <soutom@yahoo.com.br>
2020-03-08 20:10:56 -0300

    Adicionando Recyclerview Vertical para comentários

---
+++ app/src/main/java/com/example/cardviewpostagens/adapters/ComentariosAdapter.kt
@@ -0,0 +1,40 @@
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cardviewpostagens.R
+import kotlinx.android.synthetic.main.item_comentario.view.*
+
+class ComentariosAdapter(private val comentarios: List<Comentario>,
+                      private val context: Context
+) : RecyclerView.Adapter<ComentariosAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val comentario = comentarios[position]
+        holder?.let {
+            it.bindView(comentario)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_comentario, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return comentarios.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(comentario: Comentario) {
+            val comentario_view = itemView.texto_comentario
+
+            comentario_view.text = comentario.comentario
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/cardviewpostagens/view/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.cardviewpostagens.view
+
+import Comentario
+import ComentariosAdapter
+import Postagem
+import PostagemAdapter
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cardviewpostagens.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Parte do Recyclerview para postagens
+        val recyclerView = recyclerPostagem
+        recyclerView.adapter = PostagemAdapter(prepararPostagens(),this)
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = layoutManager
+
+
+        //Parte do Recyclerview para comentarios
+        val recyclerViewComentario = recyclerviewComentarios
+        recyclerViewComentario.adapter = ComentariosAdapter(preparaComentario(), this)
+        val layoutManagerComentario = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerViewComentario.layoutManager = layoutManagerComentario
+
+    }
+
+    private fun prepararPostagens(): List<Postagem>{
+
+        //Aqui poderia vir de um Banco de Dados ou da Net
+        return listOf(
+            Postagem("test1","test1",
+                R.drawable.egito
+            ),
+            Postagem("test2","test2",
+                R.drawable.paris
+            ),
+            Postagem("test3","test3",
+                R.drawable.cruseiro
+            ),
+            Postagem("test4","test4",
+                R.drawable.resort
+            )
+        )
+    }
+
+    private fun preparaComentario(): List<Comentario>{
+
+        //Aqui poderia vir de um Banco de Dados ou da Net
+        return listOf(
+            Comentario("Comemtario teste 1 Comemtario teste 1 Comemtario teste 1 " +
+                    "Comemtario teste 1 Comemtario teste 1 Comemtario teste 1 " +
+                    "Comemtario teste 1 Comemtario teste 1 Comemtario teste 1 "),
+            Comentario("Comemtario teste 2 Comemtario teste 2 Comemtario teste 2 " +
+                    "Comemtario teste 2 Comemtario teste 2 Comemtario teste 2 " +
+                    "Comemtario teste 2 Comemtario teste 2 Comemtario teste 2 "),
+            Comentario("Comemtario teste 3 Comemtario teste 3 Comemtario teste 3 " +
+                    "Comemtario teste 3 Comemtario teste 3 Comemtario teste 3 " +
+                    "Comemtario teste 3 Comemtario teste 31 Comemtario teste 3 "),
+            Comentario("Comemtario teste 1 Comemtario teste 1 Comemtario teste 1 " +
+                    "Comemtario teste 1 Comemtario teste 1 Comemtario teste 1 " +
+                    "Comemtario teste 1 Comemtario teste 1 Comemtario teste 1 "),
+            Comentario("Comemtario teste 2 Comemtario teste 2 Comemtario teste 2 " +
+                    "Comemtario teste 2 Comemtario teste 2 Comemtario teste 2 " +
+                    "Comemtario teste 2 Comemtario teste 2 Comemtario teste 2 "),
+            Comentario("Comemtario teste 3 Comemtario teste 3 Comemtario teste 3 " +
+                    "Comemtario teste 3 Comemtario teste 3 Comemtario teste 3 " +
+                    "Comemtario teste 3 Comemtario teste 31 Comemtario teste 3 "),
+            Comentario("Comemtario teste 1 Comemtario teste 1 Comemtario teste 1 " +
+                    "Comemtario teste 1 Comemtario teste 1 Comemtario teste 1 " +
+                    "Comemtario teste 1 Comemtario teste 1 Comemtario teste 1 "),
+            Comentario("Comemtario teste 2 Comemtario teste 2 Comemtario teste 2 " +
+                    "Comemtario teste 2 Comemtario teste 2 Comemtario teste 2 " +
+                    "Comemtario teste 2 Comemtario teste 2 Comemtario teste 2 "),
+            Comentario("Comemtario teste 3 Comemtario teste 3 Comemtario teste 3 " +
+                    "Comemtario teste 3 Comemtario teste 3 Comemtario teste 3 " +
+                    "Comemtario teste 3 Comemtario teste 31 Comemtario teste 3 ")
+        )
+    }
+}
